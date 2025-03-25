828b76e19cb349be219a5d434c7f0a51c09cf10c
MEGA <MEGA@gmail.com>
2025-03-25 18:03:26 +0200

    2zad

---
+++ 2 z/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_z
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            string[] words = input.Split();
+
+            foreach (string word in words)
+            {
+                if (wordFrequency.ContainsKey(word))
+                {
+                    wordFrequency[word]++;
+                }
+                else
+                {
+                    wordFrequency[word] = 1;
+                }
+            }
+
+            Console.WriteLine("\nЧестота на думите:");
+            foreach (var pair in wordFrequency)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+    }
+}
