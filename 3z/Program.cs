a0136c6d45beddc1c66af76839b2fbf5a4cfc09c
MEGA <MEGA@gmail.com>
2025-03-25 18:09:15 +0200

    3z

---
+++ 3z/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3z
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, string> employees = new SortedDictionary<int, string>();
+
+            employees.Add(103, "Peter");
+            employees.Add(112, "Maria");
+            employees.Add(102, "Ivan");
+            employees.Add(109, "Jivko");
+            employees.Add(115, "Iliqna");
+            employees.Add(104, "Maksim");
+            employees.Add(107, "Daniel");
+
+            foreach (var pair in employees)
+            {
+                Console.WriteLine($"Номер: {pair.Key}, Име: {pair.Value}");
+            }
+        }
+    }
+}
